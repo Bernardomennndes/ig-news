9936df0cd483e7f97a5b230bc988473842e5725e
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-03-02 21:29:03 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import StyleSheet from '../styles/home.module.scss';
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+      <>
+        <Head>
+          <title>ig.news</title>
+        </Head>
+
+        <h1>Hello world!</h1>
+      </>
+  );
+}
