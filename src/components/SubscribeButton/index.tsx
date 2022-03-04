378e0e66a37579425d07088308f8ea3f66483b25
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-03-04 11:34:51 -0300

    subscribe button component and main page

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,14 @@
+import styles from './style.module.scss';
+
+export function SubscribeButton() {
+    return(
+        <>
+            <button 
+                type="button"
+                className={styles.subscribeButton}
+            >   
+                Subscribe now
+            </button>
+        </>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,33 @@
-import StyleSheet from '../styles/home.module.scss';
 import Head from 'next/head';
+import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton/index'
 
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
       <>
         <Head>
-          <title>ig.news</title>
+          <title>Home | ig.news</title>
         </Head>
 
-        <h1>Hello world!</h1>
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> world</h1>
+            <p>
+              Get acess to all the publications <br />
+              <span>for $9.90 month</span>
+
+            </p>
+
+            <SubscribeButton />
+
+          </section>
+
+          <Image src="/images/avatar.svg" alt="Girl Coding" width="336" height="521" />
+
+        </main>
       </>
   );
 }
